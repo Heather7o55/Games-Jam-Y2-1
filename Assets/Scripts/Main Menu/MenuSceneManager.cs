29bb6f4b7118329a8e3ad03fc61adac34ca2adcc
ATA <aaron.bailey.168@accesscreative.ac.uk>
2025-12-06 11:18:27 +0000

    Main Menu functions working, 50% done

---
+++ Assets/Scripts/Main Menu/MenuSceneManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void MainGame()
+    {
+        SceneManager.LoadScene("Game Level");
+    }
+
+    // public void Spare()
+    // {
+    //     SceneManager.LoadScene("");
+    // }
+
+    // public void Quit()
+    // {
+    //     Application.Quit();
+    // }
+}
