1cc3b9697dbe205ce96d46bfa4a8e0a7b0588ebf
Heather <heathbeau@gmail.com>
2025-12-06 15:28:42 +0000

    Enemies almost done

--- Assets/Scripts/Entities/BasicEnemy/BasicEnemy.cs
+++ Assets/Scripts/Entities/BasicEnemy/BasicEnemy.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class BasicEnemy : BaseEntity
 {
     public int speed;
+    public Animator anim;
     private bool stunned = false;
     public float timer;
     public override void OnTakeDamage()
@@ -15,18 +16,21 @@ public class BasicEnemy : BaseEntity
     private GameObject playerObj;
     void Start()
     {
+        anim = GetComponent<Animator>();
         playerObj = GameObject.FindWithTag("Player");
     }
     void Update()
     {
         Vector3 direction = playerObj.transform.position - transform.position;
         if(stunned) return;
+        anim.Play("EnemyWalking");
         transform.position = Vector2.MoveTowards(transform.position, playerObj.transform.position, speed * Time.deltaTime);
-        float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-        selfRigidBody.rotation = angle;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, Vector2.SignedAngle(Vector2.up, direction)));
+    
     }
     public void Stun()
     {
+        anim.Play("BlockEnemy");
         StartCoroutine(CoolDown(timer));
     }
     private IEnumerator CoolDown(float timer)
